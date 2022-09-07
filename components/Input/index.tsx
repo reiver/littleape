280e235ec6a933ebead18ca5691f155fce0be8b0
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2022-09-07 19:33:47 +0430

    feat: `Input` addon support

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -3,25 +3,32 @@ import {
   FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
+  InputGroup,
+  InputLeftAddon,
   InputProps as ChakraInputProps,
+  TextareaProps,
 } from "@chakra-ui/react";
-import { forwardRef, ForwardRefRenderFunction, HTMLProps } from "react";
+import { forwardRef, ForwardRefRenderFunction, ReactNode } from "react";
 import { FieldError, FieldErrorsImpl, Merge } from "react-hook-form";
 
-export type InputProps = ChakraInputProps & {
-  name: string;
-  label?: string;
-  error?: Merge<FieldError, FieldErrorsImpl<{}>>;
-};
+export type InputProps = ChakraInputProps &
+  TextareaProps & {
+    leftAddon?: ReactNode;
+    name: string;
+    textarea?: boolean;
+    label?: string;
+    error?: Merge<FieldError, FieldErrorsImpl<{}>>;
+  };
 
 const InputComponent: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
   props,
   ref
 ) => {
-  const { error, ...rest } = props;
+  const { error, textarea, leftAddon, ...rest } = props;
+  const isInvalid = !!error;
   return (
     <FormControl
-      isInvalid={!!error}
+      isInvalid={isInvalid}
       role="group"
       display="flex"
       flexDirection="column"
@@ -46,40 +53,73 @@ const InputComponent: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
           {props.label || props.name}
         </FormLabel>
       )}
-      <ChakraInput
-        rounded="lg"
+      <InputGroup
+        role="group"
         border="1px solid"
-        borderColor="gray.200"
-        bg="light.100"
-        px="4"
-        py="2"
-        textColor="gray.800"
-        transform="200ms ease-out all"
         ring={0}
+        rounded="lg"
+        borderColor="gray.200"
+        transform="200ms ease-out all"
+        borderWidth="1px"
+        borderStyle="solid"
         _dark={{
-          bg: "dark.500",
-          textColor: "gray.50",
           borderColor: "gray.600",
           _hover: { bg: "dark.600" },
-          _focus: {
-            borderColor: "primary",
-            bg: "#393E4F",
+          _focusWithin: {
+            borderColor: "primary.500",
           },
         }}
-        _focus={{
+        _focusWithin={{
           ring: "2px",
-          borderColor: "primary",
+          borderColor: "primary.600",
         }}
-        _invalid={{
-          borderColor: "red.400 !important",
-          _focus: {
-            ringColor: "red.300",
-            borderColor: "red.500 !important",
-          },
-        }}
-        {...rest}
-        ref={ref}
-      />
+        {...(isInvalid
+          ? {
+              borderColor: "red.400 !important",
+              _focusWithin: {
+                ring: "2px",
+                ringColor: "rgba(245,101,101,.6)",
+                borderColor: "red.500 !important",
+              },
+            }
+          : {})}
+      >
+        {leftAddon && (
+          <InputLeftAddon _dark={{ bg: "dark.600" }}>
+            {leftAddon}
+          </InputLeftAddon>
+        )}
+        <ChakraInput
+          as={textarea ? "textarea" : "input"}
+          rounded="lg"
+          bg="light.100"
+          px="4"
+          py="2"
+          border="0"
+          textColor="gray.800"
+          transform="200ms ease-out all"
+          _dark={{
+            bg: "dark.500",
+            textColor: "gray.50",
+            _hover: { bg: "dark.600" },
+            _focus: {
+              bg: "#393E4F",
+            },
+          }}
+          _focus={{
+            ring: "0",
+          }}
+          _invalid={{
+            borderColor: "red.400 !important",
+            _focusWithin: {
+              ringColor: "red.300",
+              borderColor: "red.500 !important",
+            },
+          }}
+          {...rest}
+          ref={ref}
+        />
+      </InputGroup>
 
       {error && (
         <FormErrorMessage fontSize="sm">{error.message}</FormErrorMessage>
