fb8c000be3212c043be91a39e42e9aff1740cb32
Nariman Movaffaghi <nariman.movaffaghi@gmail.com>
2022-09-07 23:41:18 +0430

    feat: rewrite activity requests to backend

--- next.config.js
+++ next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 
+const { joinURL } = require("ufo");
+
 const nextConfig = {
   swcMinify: true,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/u/:path*",
+          has: [
+            {
+              type: "header",
+              key: "accept",
+              value: "application/activity\\+json",
+            },
+          ],
+          destination: joinURL(process.env.NEXT_PUBLIC_HOST, "/u/:path*"),
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
