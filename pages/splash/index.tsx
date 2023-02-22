1b0de21364a81ad9f29218c00906455d9523cfe9
nastaran1999 <nastaran_78_ab@yahoo.com>
2023-02-22 17:16:01 +0330

    splash page added.

---
+++ pages/splash/index.tsx
@@ -0,0 +1,12 @@
+
+import styles from '../../styles/Splash.module.scss';
+
+export default function Splash() {
+  return (
+    <div className={styles.splashContainer}>
+      <img className={styles.splashLogo} src="/greatape-logo.svg" />
+      <h2 className={styles.splashTitle}>GreatApe</h2>
+    </div>
+  );
+}
+
