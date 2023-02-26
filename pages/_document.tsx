b1dec75a5f2adbb601e7d0f8ac20d66a663a0a84
nastaran1999 <nastaran_78_ab@yahoo.com>
2023-02-26 13:49:19 +0330

    Open Sans font variants added.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"            
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
