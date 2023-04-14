35af9ea7d893dbb0fe963634e7689962134506c8
nastaran1999 <nastaran_78_ab@yahoo.com>
2023-04-14 14:35:49 +0330

    splash screen styles adapted with chakraUI.

--- pages/splash/index.tsx
+++ pages/splash/index.tsx
@@ -1,13 +1,20 @@
-
-import styles from '../../styles/Splash.module.scss';
+import { Flex, Image } from '@chakra-ui/react';
 
 export default function Splash() {
   return (
-    <div className={styles.splashContainer}>
-      <img className={styles.splashLogo} src="/greatape-logo.svg" />
-      <h2 className={styles.splashTitle}>GreatApe</h2>
-      <p className={styles.splashVersion}>version 1</p>
-    </div>
+    <Flex 
+      bg='#FFCC00'
+      display="flex"
+      width="100%"
+      height="100%"
+      position="fixed"
+    >
+      <Image 
+        src="/logo.svg"
+        width="auto"
+        height="57%"
+        margin="auto"
+      />
+    </Flex>
   );
 }
-
