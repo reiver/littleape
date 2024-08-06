86ee5b3f166ebd581d45ef2d95331b960aa3339e
Mian-Zaid <zaid7296@gmail.com>
2024-08-06 11:32:05 +0500

    hard coded domain in config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { ThirdwebProvider, WalletProvider } from "web3-wallet-connection";
 
 import { AuthKitProvider } from '@farcaster/auth-kit';
-import '@farcaster/auth-kit/styles.css';
 import {
   QueryClient
 } from '@tanstack/react-query';
@@ -16,11 +15,12 @@ import { useAuthStore } from "store";
 import "styles/global.css";
 import { SWRConfig } from "swr";
 import "../styles/styles.css";
+import '@farcaster/auth-kit/styles.css';
 
 const config = {
   rpcUrl: 'https://mainnet.optimism.io',
-  domain: `${window.location.hostname}`,
-  siweUri: `https://${window.location.hostname}/auth/login`,
+  domain: 'littleape-swart.vercel.app',
+  siweUri: 'https://littleape-swart.vercel.app/auth/login',
 };
 
 dayjs.extend(relativeTime);
