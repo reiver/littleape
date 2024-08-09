ff680cc3f7fd9806f85b9c7075713836495deafb
Mian-Zaid <zaid7296@gmail.com>
2024-08-09 12:41:43 +0500

    fix: display QR code in modal

--- components/SignInWithFarcaster/index.tsx
+++ components/SignInWithFarcaster/index.tsx
@@ -1,7 +1,10 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Button, Modal, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, ModalProps, Text, useDisclosure } from "@chakra-ui/react";
 import { QRCode, useSignIn } from '@farcaster/auth-kit';
-import { useState } from "react";
+import { FC, useState } from "react";
+import CopyIcon from '../../public/Copy.svg';
+import LinkIcon from '../../public/Link.svg';
 import styles from "./MyComponent.module.css";
+
 
 import { createAppClient, viemConnector } from '@farcaster/auth-client';
 
@@ -14,6 +17,12 @@ export const SignInWithFarcasterButton = ({ onSuccess, onError }) => {
     const [url, setUrl] = useState(null)
     let shouldContinueChecking = true;
 
+    const {
+        isOpen: isShowQRCodeModalOpen,
+        onOpen: onShowQRCodeModalOpen,
+        onClose: onShowQRCodeModalClose,
+    } = useDisclosure();
+
     const createChannel = async () => {
         const { data } = await appClient.createChannel({
             siweUri: "https://littleape-swart.vercel.app/auth/login",
@@ -22,6 +31,9 @@ export const SignInWithFarcasterButton = ({ onSuccess, onError }) => {
 
         console.log("Client data: ", data)
         setUrl(data.url)
+
+        // Open QR Code Modal when URL is set
+        onShowQRCodeModalOpen();
 
         checkStatusContinuously(data.channelToken);
 
@@ -89,12 +101,87 @@ export const SignInWithFarcasterButton = ({ onSuccess, onError }) => {
             {url && (
 
                 <div>
-                    <Text className={styles.largeText}>Sign in with Farcaster</Text>
+                    {/* <Text className={styles.largeText}>Sign in with Farcaster</Text>
                     <Text className={styles.smallText}>Scan with your phone's camera to continue.</Text>
-                    <QRCode uri={url} size={310} />
+                    <QRCode uri={url} size={310} /> */}
+
+                    <ShowQRCodeModal
+                        url={url}
+                        isOpen={isShowQRCodeModalOpen}
+                        onClose={() => {
+                            onShowQRCodeModalClose()
+                            setUrl(null)
+                        }}
+                    />
                 </div>
+
+
             )}
         </div>
 
+    )
+}
+
+function trimUrl(url, maxLength) {
+    // Ensure maxLength is a positive number
+    if (maxLength <= 0) {
+        throw new Error("maxLength should be a positive number");
+    }
+
+    // Trim leading and trailing whitespace
+    const trimmedUrl = url.trim();
+
+    // Check if the URL length exceeds the maximum length
+    if (trimmedUrl.length > maxLength) {
+        // Trim the URL and add ellipsis
+        return trimmedUrl.slice(0, maxLength - 3) + '...';
+    }
+
+    // Return the original URL if it doesn't exceed maxLength
+    return trimmedUrl;
+}
+
+
+const copyToClipboard = (address: string) => {
+    navigator.clipboard.writeText(address)
+};
+
+type ShowQRCodeModalProps = { url: string } & Omit<ModalProps, "children">;
+
+const ShowQRCodeModal: FC<ShowQRCodeModalProps> = ({ isOpen, onClose, url, ...props }) => {
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose} {...props}>
+            <ModalOverlay />
+            <ModalContent mx="3" _dark={{ bg: "dark.700" }} className={styles.modelContent}>
+                <ModalHeader className={styles.centerDiv}
+                    px={{
+                        base: "4",
+                        md: "6",
+                    }}
+                >
+                    <Text className={styles.textHeading} fontSize={{ base: "md", md: "inherit" }}>Login With Farcaster</Text>
+                </ModalHeader>
+                <div className={styles.QRCode}>
+                    <QRCode uri={url} size={250} />
+                </div>
+                <div className={styles.centerDiv}>
+                    <Text className={styles.smallText}>Scan the QR code with your phone or enter the link on your browser</Text>
+                </div>
+                <ModalCloseButton />
+                <ModalFooter>
+                    <div className={styles.linkBackground}>
+                        <LinkIcon />
+                        <Text className={styles.linkText}>${trimUrl(url, 29)}</Text>
+                        <Button
+                            onClick={() => copyToClipboard(url)}
+                            className={styles.iconButton}
+                        >
+                            <CopyIcon className={styles.icon} />
+                        </Button>
+                    </div>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
     )
 }
